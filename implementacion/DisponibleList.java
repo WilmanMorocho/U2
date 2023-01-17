6a7d7b0cfd73cc565e99bcb7a0fc363b7afd5281
WilmanMorocho <117868799+WilmanMorocho@users.noreply.github.com>
2023-01-16 19:17:56 -0500

    Initial commit

---
+++ implementacion/DisponibleList.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package evaluacion2u;
+
+/**
+ *
+ * @author Usuario Pc
+ */
+public class DisponibleList extends PelisWeb{
+    
+    private String encabezado;
+    private String peliculaDisponibleList;
+    
+    public static void mostrarPeliculasDisponible(String peliculaDisponibleList){
+    
+    }
+}
